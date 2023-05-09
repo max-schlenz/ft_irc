983d6bc97a299a303495a71d3d7d178e38729fc9
max-schlenz <mschlenz@student.42heilbronn.de>
2023-05-09 12:36:27 +0200

    server constructor

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,4 +1,5 @@
 # include <Server.hpp>
+# include <Client.hpp>
 
 void	exiting(int error_code);
 
@@ -16,4 +17,21 @@ void Server::accept_client()
 		std::cout << GREEN << "Client " << BGREEN << client.ipStr() << GREEN << " connected." << RESET << std::endl;
 		this->_clients.push_back(client);
 	}
+}
+
+Server::Server()
+{
+	this->_proto = getprotobyname("tcp");
+	this->_sock = socket(AF_INET, SOCK_STREAM, this->_proto->p_proto);
+	this->_port = 6667;
+	this->_sin.sin_family = AF_INET;
+	this->_sin.sin_port = htons(this->_port);
+	this->_sin.sin_addr.s_addr = htonl(INADDR_ANY);
+	this->_sinLen = sizeof(this->_sin);
+
+	int optval = 1;
+    setsockopt(this->_sock, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+	fcntl(this->_sock, F_SETFL, O_NONBLOCK);
+	bind(this->_sock, (struct sockaddr*)&this->_sin, this->_sinLen);
+	listen(this->_sock, 5);
 }
