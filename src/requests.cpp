42610331cb78a3b4af7e0db1baffc9c40f433cdd
Dennis Antonik <dantonik@2-F-7.42heilbronn.de>
2023-05-14 16:32:00 +0200

    welcome message

--- src/requests.cpp
+++ src/requests.cpp
@@ -2,7 +2,9 @@
 
 void Server::handleReqHandshake(Client& client, std::vector<std::string> reqVec)
 {
-	std::string response = ":127.0.0.1 001 mschlenz :Welcome to the Internet Relay Network mschlenz!mschlenz@mschlenz\r\n";
+	std::string response = ":127.0.0.1 001 mschlenz :\002\037\0038Welcome to our dmt.irc server!\002\037\0038\r\n";
+	send(client.getSock(), response.c_str(), response.size(), 0);
+	response = "\033[0;34m[CAP LS]\033[0m no available capabilities\r\n";
 	send(client.getSock(), response.c_str(), response.size(), 0);
 }
 
@@ -16,6 +18,10 @@ void Server::handleReqPing(Client& client, std::vector<std::string> reqVec)
 	}
 }
 
+//MUST NOT contain any of the following characters: space (' ', 0x20), comma (',', 0x2C), asterisk ('*', 0x2A), question mark ('?', 0x3F), exclamation mark ('!', 0x21), at sign ('@', 0x40)
+//MUST NOT start with any of the following characters: dollar ('$', 0x24), colon (':', 0x3A)
+//MUST NOT start with a character listed as a channel type prefix
+//SHOULD NOT contain any dot character ('.', 0x2E)
 void Server::handleReqNick(Client& client, std::vector<std::string> reqVec)
 {
 	if (reqVec.size() > 1)
