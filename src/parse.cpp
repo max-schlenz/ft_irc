647aa4c4b643d94314429271d5fe50b167368795
Dennis Antonik <dantonik@2-F-5.42heilbronn.de>
2023-05-19 11:50:08 +0200

    update parsing

--- src/parse.cpp
+++ src/parse.cpp
@@ -66,5 +66,47 @@ bool checkPart(std::vector<std::string> reqVec, Client& client, std::vector<Chan
 		send(client.getSock(), err_msg.c_str(), err_msg.size(), 0);
 		return false;
 	}
+	if (!channelExists(channelToPart, client.getJoinedChannels())) {
+		err_msg = ERR_NOTONCHANNEL + clientIp + channelToPart + " :You're not on that channel";
+		send(client.getSock(), err_msg.c_str(), err_msg.size(), 0);
+		return false;
+	}
 	return true;
+}
+
+
+bool checkInvite(std::vector<std::string> reqVec, Client& client, std::vector<Channel> channels) {
+	std::string clientIp = client.getIpStr() + " ";
+	std::string err_msg;
+	if (reqVec.size() < 2) {
+		err_msg = ERR_NEEDMOREPARAMS + clientIp + reqVec[0] + " :Not enough parameters";
+		send(client.getSock(), err_msg.c_str(), err_msg.size(), 0);
+		return false;
+	}
+	std::string channelToPart = reqVec[2].replace(0, 1, "#");
+	if (!channelExists(channelToPart, channels)) {
+		err_msg = ERR_NOSUCHCHANNEL + channelToPart + " :No such channel";
+		send(client.getSock(), err_msg.c_str(), err_msg.size(), 0);
+		return false;
+	}
+	if (/*member not on channel*/) {
+		err_msg = ERR_NOTONCHANNEL + clientIp + channelToPart + " :You're not on that channel";
+		send(client.getSock(), err_msg.c_str(), err_msg.size(), 0);
+		return false;
+	}
+	if (/*invite only channel & client not operator*/) {
+		err_msg = ERR_CHANOPRIVSNEEDED + clientIp + channelToPart + " :You're not channel operator";
+		send(client.getSock(), err_msg.c_str(), err_msg.size(), 0);
+		return false;
+	}
+	if (/*user to be invited is already on channel*/) {
+		err_msg = ERR_USERONCHANNEL + clientIp  + client.getNickname() + " " + channelToPart + " :is already on channel";
+		send(client.getSock(), err_msg.c_str(), err_msg.size(), 0);
+		return false;
+	}
+	return true;
+}
+
+bool checkKick(std::vector<std::string> reqVec, Client& client, std::vector<Channel> channels) {
+
 }
