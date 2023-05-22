63e35c7caa70daadaaa57a64fcad71bcbbaf3b94
Maximilian Schlenz <mschlenz@student.42heilbronn.de>
2023-05-22 12:38:28 +0000

    fixed CAP

--- src/commands/capreq.cpp
+++ src/commands/capreq.cpp
@@ -2,8 +2,17 @@
 
 void Server::capreq(std::vector<std::string> reqVec, Client &client)
 {
-	std::string response = ":127.0.0.1 CAP * LS :multi-prefix away-notify account-notify\r\n";
-	send(client.getSock(), response.c_str(), response.size(), 0);
-	response = ":127.0.0.1 CAP * ACK :multi-prefix away-notify account-notify\r\n";
-	send(client.getSock(), response.c_str(), response.size(), 0);
+	if (reqVec.size() > 1)
+	{
+		if (reqVec[1] == "LS")
+		{
+			std::string response = ":127.0.0.1 CAP * LS :multi-prefix away-notify account-notify\r\n";
+			send(client.getSock(), response.c_str(), response.size(), 0);
+		}
+		else if (reqVec[1] == "REQ")
+		{
+			std::string response = ":127.0.0.1 CAP * ACK :multi-prefix away-notify account-notify\r\n";
+			send(client.getSock(), response.c_str(), response.size(), 0);
+		}
+	}
 }
--- src/commands/nick.cpp
+++ src/commands/nick.cpp
@@ -19,7 +19,6 @@ void Server::nick(std::vector<std::string> reqVec, Client &client)
 			send(client.getSock(), response.c_str(), response.size(), 0);
 			client.setRegistered(true);
 			this->_clientsM[client.getNickname()] = &client;
-			PRNT;
 		}
 	}
 }
