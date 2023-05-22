c78a51fd32ef31d47a9c5b960fde56c8d9d98079
Maximilian Schlenz <mschlenz@student.42heilbronn.de>
2023-05-22 12:33:52 +0000

    removed debug print statement in capreq

--- src/commands/capreq.cpp
+++ src/commands/capreq.cpp
@@ -6,9 +6,4 @@ void Server::capreq(std::vector<std::string> reqVec, Client &client)
 	send(client.getSock(), response.c_str(), response.size(), 0);
 	response = ":127.0.0.1 CAP * ACK :multi-prefix away-notify account-notify\r\n";
 	send(client.getSock(), response.c_str(), response.size(), 0);
-	std::cout << GRAY;
-	for (std::vector<std::string>::iterator it = reqVec.begin(); it != reqVec.end(); ++it)
-		std::cout << *it + " ";
-	std::cout << RESET << std::endl;
-	
 }
