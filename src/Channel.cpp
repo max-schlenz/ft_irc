a021d02af97cd9c169275d838f817aab3501ef5c
TaleaX <sophie.dehne01@gmail.com>
2023-05-24 17:56:00 +0200

    no password

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -2,7 +2,7 @@
 #include "Channel.hpp"
 
 Channel::Channel(std::string name):
-	_name(name), _topic(""), _password("1234"), _modes(""), mode_i(false), mode_t(false), mode_k(true), mode_o(false)
+	_name(name), _topic(""), _password(""), _modes(""), mode_i(false), mode_t(false), mode_k(false), mode_o(false)
 {
 	std::cout << "channel: " << name << " created" << std::endl;
 }
